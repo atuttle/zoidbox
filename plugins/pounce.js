9a22b22cac37180b9a8b70558225995050ae02a4
Ryan Guill <ryanguill@gmail.com>
2015-01-26 18:56:59 -0600

    add timestamp to pounce https://github.com/atuttle/zoidbox/issues/18

--- plugins/pounce.js
+++ plugins/pounce.js
@@ -4,6 +4,7 @@ module.exports = (function(){
 
 	var bot;
 	var _ = require( 'lodash' );
+	var moment = require('moment');
 
 	return function init( _bot ){
 		bot = _bot;
@@ -72,7 +73,7 @@ module.exports = (function(){
 
 	function addPounce( chan, nick, from, msg ){
 		bot.redis.sadd( chan + '.pouncees', nick.toLowerCase() );
-		bot.redis.sadd( chan + '.pounce.' + nick.toLowerCase(), JSON.stringify({ from: from, msg: msg }) );
+		bot.redis.sadd( chan + '.pounce.' + nick.toLowerCase(), JSON.stringify({ from: from, msg: msg, time: new Date().getTime() }) );
 		return true;
 	}
 
@@ -99,7 +100,8 @@ module.exports = (function(){
 							console.log(pounces[p]);
 							var data = JSON.parse( pounces[p] );
 							console.log(data);
-							bot.say( chan, nick + ': ' + data.from + ' wanted me to tell you, `' + data.msg + '`');
+
+							bot.say( chan, nick + ': ' + data.from + ' wanted me to tell you, `' + data.msg + '` ~ sent: ' + moment(data.time).toISOString());
 						}
 						bot.redis.del( chan + '.pounce.' + nick );
 						bot.redis.srem( chan + '.pouncees', nick );
