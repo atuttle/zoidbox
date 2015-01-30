966e5be2cb0993ce8cfc0774ecb70d5d18750b02
Matt Gifford <me@mattgifford.co.uk>
2015-01-30 16:05:58 +0000

    Update cfdocs.js
    Out, whitespace, out!

--- plugins/cfdocs.js
+++ plugins/cfdocs.js
@@ -9,20 +9,20 @@ module.exports = (function(){
 		redis,
 		log,
 		conf,
-				queryExceptions = {
-						fw1: 'http://framework-one.github.io/documentation/',
-						di1: 'https://github.com/framework-one/di1/wiki (will change later)',
-						taffy: 'http://docs.taffy.io',
-						coldbox: 'http://wiki.coldbox.org/',
-						testbox: 'http://wiki.coldbox.org/wiki/TestBox.cfm',
-						wirebox: 'http://wiki.coldbox.org/wiki/WireBox.cfm',
-						commandbox: 'http://www.ortussolutions.com/products/commandbox/docs/current',
-						cfclient: '<cfclient></cfclient> → returns a pink slip, because if you use this shit you should be fired. ~ http://www.codecademy.com/en/tracks/javascript',
-						cf_socialplugin: '<cf_socialplugin .. /> → returns a bunch of outdated junk that would have been better as a community project dear god what have we done... we should have just given them a package manager like they\'ve been requesting for years ~ http://cfdocs.org/cf_socialplugin',
-						cfscriptref: 'https://github.com/daccfml/cfscript/blob/master/cfscript.md',
-						cfdownloads: 'http://www.gpickin.com/cfrepo/',
-						monkehtweets: 'https://github.com/coldfumonkeh/monkehTweets'
-				}; //make sure keys are completely lowercase
+        queryExceptions = {
+            fw1: 'http://framework-one.github.io/documentation/',
+            di1: 'https://github.com/framework-one/di1/wiki (will change later)',
+            taffy: 'http://docs.taffy.io',
+            coldbox: 'http://wiki.coldbox.org/',
+            testbox: 'http://wiki.coldbox.org/wiki/TestBox.cfm',
+            wirebox: 'http://wiki.coldbox.org/wiki/WireBox.cfm',
+            commandbox: 'http://www.ortussolutions.com/products/commandbox/docs/current',
+            cfclient: '<cfclient></cfclient> → returns a pink slip, because if you use this shit you should be fired. ~ http://www.codecademy.com/en/tracks/javascript',
+            cf_socialplugin: '<cf_socialplugin .. /> → returns a bunch of outdated junk that would have been better as a community project dear god what have we done... we should have just given them a package manager like they\'ve been requesting for years ~ http://cfdocs.org/cf_socialplugin',
+            cfscriptref: 'https://github.com/daccfml/cfscript/blob/master/cfscript.md',
+            cfdownloads: 'http://www.gpickin.com/cfrepo/',
+            monkehtweets: 'https://github.com/coldfumonkeh/monkehTweets'
+        }; //make sure keys are completely lowercase
 
 	return function init( _bot ){
 		bot = _bot;
@@ -32,60 +32,60 @@ module.exports = (function(){
 
 		bot.on( 'message', function( from, to, text, message ){
 
-						if (bot.isChannelPaused(to)) return;
+            if (bot.isChannelPaused(to)) return;
 
-						if (to === bot.botName) {
+            if (to === bot.botName) {
 				//they are talking to us in a private message, set to to be from
 				to = from;
 			}
 
-						var parts = text.trim().split(' ');
+            var parts = text.trim().split(' ');
 
 			if (text.indexOf('!') === 0){
 				log('cfdocs', from, to, text, message.user, message.nick);
-								if (parts.length > 2 && parts[1] === '!desc') {
-										if (parts[0].slice(1) === from) {
-												if ('~' + message.nick.toLowerCase().substr(0,9) === message.user.toLowerCase()) {
-														setDesc(parts[0].slice(1), parts.slice(2, parts.length).join(' '));
-														bot.say(to, 'New description saved for `' + parts[0].slice(1) + '`');
-												} else {
-														bot.say(to, 'You can only set a description for your nick if you are authenticated and are using your username as your nickname.  Username: ' + message.user);
-												}
-										} else {
-												bot.ops.isOp(message.user, function (err, data) {
-														if (data === 0) {
-																bot.say(to, 'You must be an op to do that.');
-														} else {
-																if (parts[2] === '!clear') {
-																		clearDesc(parts[0].slice(1));
-																		bot.say(to, 'Description cleared for `' + parts[0].slice(1) + '`');
-																} else {
-																		setDesc(parts[0].slice(1), parts.slice(2, parts.length).join(' '));
-																		bot.say(to, 'New description saved for `' + parts[0].slice(1) + '`');
-																}
-														}
-												});
-										}
-								} else {
-										return docs(to, text.slice(1));
-								}
+                if (parts.length > 2 && parts[1] === '!desc') {
+                    if (parts[0].slice(1) === from) {
+                        if ('~' + message.nick.toLowerCase().substr(0,9) === message.user.toLowerCase()) {
+                            setDesc(parts[0].slice(1), parts.slice(2, parts.length).join(' '));
+                            bot.say(to, 'New description saved for `' + parts[0].slice(1) + '`');
+                        } else {
+                            bot.say(to, 'You can only set a description for your nick if you are authenticated and are using your username as your nickname.  Username: ' + message.user);
+                        }
+                    } else {
+                        bot.ops.isOp(message.user, function (err, data) {
+                            if (data === 0) {
+                                bot.say(to, 'You must be an op to do that.');
+                            } else {
+                                if (parts[2] === '!clear') {
+                                    clearDesc(parts[0].slice(1));
+                                    bot.say(to, 'Description cleared for `' + parts[0].slice(1) + '`');
+                                } else {
+                                    setDesc(parts[0].slice(1), parts.slice(2, parts.length).join(' '));
+                                    bot.say(to, 'New description saved for `' + parts[0].slice(1) + '`');
+                                }
+                            }
+                        });
+                    }
+                } else {
+                    return docs(to, text.slice(1));
+                }
 			} else if (text.indexOf('#cfdocs') === 0 && text.split(' ').length > 1) {
-								log('#cfdocs', from, to, text);
-								switch (parts[1]) {
-										case '!stats' :
-												getTotalHits(to, function(err, data) {
-														if (err) {
-																log('getTotalHits err', err, data);
-														}
+                log('#cfdocs', from, to, text);
+                switch (parts[1]) {
+                    case '!stats' :
+                        getTotalHits(to, function(err, data) {
+                            if (err) {
+                                log('getTotalHits err', err, data);
+                            }
 
-														var totalHits = _.parseInt((_.isNull(data) ? 0 : data), 10);
+                            var totalHits = _.parseInt((_.isNull(data) ? 0 : data), 10);
 
-														getLeaderboard(to, function(err, data) {
-																if (err) {
-																		log('getLeaderboard err', err, data);
-																}
+                            getLeaderboard(to, function(err, data) {
+                                if (err) {
+                                    log('getLeaderboard err', err, data);
+                                }
 
-																var leaders = _.map(
+                                var leaders = _.map(
 										_.sortBy(
 											_.filter(
 													_.map(data, function(item, key) {
@@ -98,122 +98,122 @@ module.exports = (function(){
 									})
 								.reverse()
 								.slice(0, 15),
-																function(item) {
+                                function(item) {
 									return item[0] + ': ' + item[1] + ' (' + _.parseInt((item[1] / totalHits) * 100, 10) + '%)';
 								}).join(', ');
 
-											bot.say(to, totalHits + ' total CFDocs search' + ( totalHits !== 1 ? 'es' : '') + '. Most searched queries: ' + leaders);
-														});
-												});
+					            bot.say(to, totalHits + ' total CFDocs search' + ( totalHits !== 1 ? 'es' : '') + '. Most searched queries: ' + leaders);
+                            });
+                        });
 
-												break;
-										case '!reset' :
-												bot.ops.isOp(message.user, function(err, data){
-														if (data === 0) {
-																bot.say(to, 'You must be an op to do that.');
-														} else {
-																resetHits(to);
-															bot.say(to, 'CFDocs stats have been reset.');
-														}
-												});
-												break;
-										default :
-												getHits(to, parts[1], function(err, data) {
-														if (err) {
-																log('getHits err:', err, data);
-																return;
-														}
+                        break;
+                    case '!reset' :
+                        bot.ops.isOp(message.user, function(err, data){
+                            if (data === 0) {
+                                bot.say(to, 'You must be an op to do that.');
+                            } else {
+                                resetHits(to);
+                               bot.say(to, 'CFDocs stats have been reset.');
+                            }
+                        });
+                        break;
+                    default :
+                        getHits(to, parts[1], function(err, data) {
+                            if (err) {
+                                log('getHits err:', err, data);
+                                return;
+                            }
 
-														if (_.parseInt(data, 10) > 0) {
-																bot.say(to, parts[1] + ' has been searched for ' + data + ' time' + (_.parseInt(data, 10) !== 1 ? 's.' : '.'));
-														} else {
-																bot.say(to, parts[1] + ' has never been searched for.');
-														}
-												});
-												break;
-								}
-						}
+                            if (_.parseInt(data, 10) > 0) {
+                                bot.say(to, parts[1] + ' has been searched for ' + data + ' time' + (_.parseInt(data, 10) !== 1 ? 's.' : '.'));
+                            } else {
+                                bot.say(to, parts[1] + ' has never been searched for.');
+                            }
+                        });
+                        break;
+                }
+            }
 		});
 	};
 
-		function loghit (channel, q) {
+    function loghit (channel, q) {
 		redis.hincrby(conf.get('botName') + '.' + channel + '.cfdocs_hits', q.toLowerCase(), 1);
 		redis.hincrby(conf.get('botName') + '.' + channel + '.cfdocs_hits', 'TOTAL', 1);
 	}
 
-		function setDesc (q, desc) {
-				redis.hset(conf.get('botName') + '.customDescriptions', q.toLowerCase(), desc);
-		}
+    function setDesc (q, desc) {
+        redis.hset(conf.get('botName') + '.customDescriptions', q.toLowerCase(), desc);
+    }
 
-		function getDesc (q, callback) {
-				redis.hget(conf.get('botName') + '.customDescriptions', q.toLowerCase(), callback);
-		}
+    function getDesc (q, callback) {
+        redis.hget(conf.get('botName') + '.customDescriptions', q.toLowerCase(), callback);
+    }
 
-		function clearDesc (q) {
-				redis.hdel(conf.get('botName') + '.customDescriptions', q.toLowerCase());
-		}
+    function clearDesc (q) {
+        redis.hdel(conf.get('botName') + '.customDescriptions', q.toLowerCase());
+    }
 
-		function getHits (channel, q, callback) {
-				redis.hget(conf.get('botName') + '.' + channel + '.cfdocs_hits', q.toLowerCase(), callback);
-		}
+    function getHits (channel, q, callback) {
+        redis.hget(conf.get('botName') + '.' + channel + '.cfdocs_hits', q.toLowerCase(), callback);
+    }
 
-		function getTotalHits (channel, callback) {
-				redis.hget(conf.get('botName') + '.' + channel + '.cfdocs_hits', 'TOTAL', callback);
-		}
+    function getTotalHits (channel, callback) {
+        redis.hget(conf.get('botName') + '.' + channel + '.cfdocs_hits', 'TOTAL', callback);
+    }
 
-		function getLeaderboard(channel, callback) {
+    function getLeaderboard(channel, callback) {
 		redis.hgetall(conf.get('botName') + '.' + channel + '.cfdocs_hits', callback);
 	}
 
-		function resetHits(channel) {
+    function resetHits(channel) {
 		redis.del(conf.get('botName') + '.' + channel + '.cfdocs_hits');
 	}
 
 	function docs (channel, q){
 		q = q.toLowerCase();
-				loghit(channel, q);
-				getHits(channel, q, function(err, data) {
-						if (err) {
-								log('getHits err:', err, data);
-								return;
-						}
+        loghit(channel, q);
+        getHits(channel, q, function(err, data) {
+            if (err) {
+                log('getHits err:', err, data);
+                return;
+            }
 
-						if (queryExceptions.hasOwnProperty(q)) {
-								return bot.say(channel, queryExceptions[q]);
-						}
+            if (queryExceptions.hasOwnProperty(q)) {
+                return bot.say(channel, queryExceptions[q]);
+            }
 
-						getDesc(q, function(err, result) {
+            getDesc(q, function(err, result) {
 
-								if (result !== null) {
-										bot.say(channel, result);
-								} else {
-										docsApi( q, function(err, result){
-												if (err !== null){
-														bot.say(channel, err );
-												} else {
-														var theoreticalMax = 400; //rough guess at how many characters we get
-														var link = ' ~ http://cfdocs.org/' + q;
-														var msg = '';
-														if (result.type === 'tag'){
-																msg = result.syntax + ' → ' + result.description.replace(/\s+/g, ' ');
-														}else{
-																msg = result.syntax + ' → returns ' + ( result.returns.length ? result.returns : ' nothing' );
-														}
+                if (result !== null) {
+                    bot.say(channel, result);
+                } else {
+                    docsApi( q, function(err, result){
+                        if (err !== null){
+                            bot.say(channel, err );
+                        } else {
+                            var theoreticalMax = 400; //rough guess at how many characters we get
+                            var link = ' ~ http://cfdocs.org/' + q;
+                            var msg = '';
+                            if (result.type === 'tag'){
+                                msg = result.syntax + ' → ' + result.description.replace(/\s+/g, ' ');
+                            }else{
+                                msg = result.syntax + ' → returns ' + ( result.returns.length ? result.returns : ' nothing' );
+                            }
 
-														var bufferRemaining = theoreticalMax - ( (conf.get('botName').length + 1) + link.length);
-														var fitMsg = msg.substr(0, bufferRemaining);
-														if (fitMsg !== msg){
-																fitMsg = fitMsg + '…';
-														}
-														fitMsg = fitMsg + link;
-														bot.say( channel, fitMsg );
-												}
-										});
-								}
-						});
+                            var bufferRemaining = theoreticalMax - ( (conf.get('botName').length + 1) + link.length);
+                            var fitMsg = msg.substr(0, bufferRemaining);
+                            if (fitMsg !== msg){
+                                fitMsg = fitMsg + '…';
+                            }
+                            fitMsg = fitMsg + link;
+                            bot.say( channel, fitMsg );
+                        }
+                    });
+                }
+            });
 
 
-				});
+        });
 
 
 
@@ -225,12 +225,12 @@ module.exports = (function(){
 
 		request(full, function (error, response, body) {
 			if ( !error && response.statusCode === 200 ) {
-								try {
-										var r = JSON.parse(body);
-										callback(null, r);
-								} catch (e) {
-										callback('Error Parsing JSON for `' + q + '`', null);
-								}
+                try {
+                    var r = JSON.parse(body);
+                    callback(null, r);
+                } catch (e) {
+                    callback('Error Parsing JSON for `' + q + '`', null);
+                }
 			} else if ( response.statusCode === 404 ){
 				callback( 'Unable to find docs for `' + q + '`', null );
 			}
