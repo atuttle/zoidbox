7423028095aa88050d1c8e7781b28d962f3a48bc
Adam Tuttle <adam@fusiongrokker.com>
2015-02-02 08:50:23 -0500

    only do isup checks for valid urls. resolves #27

--- plugins/isup.js
+++ plugins/isup.js
@@ -4,6 +4,7 @@ module.exports = (function(){
 
 	var _bot;
 	var isup = require('is-up');
+	var validator = require('valid-url');
 
 	return function init( bot ){
 		_bot = bot;
@@ -19,9 +20,13 @@ module.exports = (function(){
 			if (text.indexOf('^') === 0 && text.length >= 4 && text.split(' ').length === 1) {
 				var url = text.split('');
 				url.shift();
-				url = url.join('').toLowerCase();
-				url = url.replace('https://', '');
-				url = url.replace('http://', '');
+				url = url.join('').toLowerCase().replace('https://', '').replace('http://', '');
+
+				if (!validator.isWebUri( 'http://' + url )){
+					//fail silently since this doesn't appear to be a url
+					return;
+				}
+
 				isup(url, function(err, up){
 					bot.say(to, url + ' is ' + (up ? 'up' : 'down') + ' for me' + (up ? '...' : ' too.') );
 				});
